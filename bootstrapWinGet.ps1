2cabb95cedcd21440a83a5a9dee9c8248887c7e0
Clint Rutkas <clint@rutkas.com>
2024-02-01 23:37:50 -0800

    making it so script gets output

--- bootstrapWinGet.ps1
+++ bootstrapWinGet.ps1
@@ -7,40 +7,40 @@ if(!(($isWinGetRecent[0] -gt 1) -or ($isWinGetRecent[0] -ge 1 -and $isWinGetRece
 {
    $paths = "Microsoft.VCLibs.x64.14.00.Desktop.appx", "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle", "Microsoft.UI.Xaml.2.7.x64.appx"
    $uris = "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx", "https://aka.ms/getwinget", "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx"
-   Write-Information "Downloading WinGet and its dependencies..."
+   Write-Host "Downloading WinGet and its dependencies..."
 
    for ($i = 0; $i -lt $uris.Length; $i++) {
        $filePath = $paths[$i]
        $fileUri = $uris[$i]
-       Write-Information "Downloading: " + $filePat + " from " + $fileUri
-       Invoke-WebRequest -Uri $fileUri -OutFile $filePat
+       Write-Host "Downloading: ($filePat) from $fileUri"
+       Invoke-WebRequest -Uri $fileUri -OutFile $filePath
    }
 
-   Write-Information "Installing WinGet and its dependencies..."
+   Write-Host "Installing WinGet and its dependencies..."
    
    foreach($filePath in $paths)
    {
-       Write-Information "Installing: " + $filePath
-       Add-AppxPackage $$filePath
+       Write-Host "Installing: ($filePath)"
+       Add-AppxPackage $filePath
    }
 
-   Write-Information "Verifying Version number of WinGet"
+   Write-Host "Verifying Version number of WinGet"
    winget -v
 
-   Write-Information "Cleaning up"
+   Write-Host "Cleaning up"
    foreach($filePath in $paths)
    {
       if (Test-Path $filePath) 
       {
-         Write-Information "Deleting: " + $filePath
+         Write-Host "Deleting: ($filePath)"
          Remove-Item $filePath -verbose
       } 
       else
       {
-         Write-Error "Path doesn't exits: " + $filePath
+         Write-Error "Path doesn't exits: ($filePath)"
       }
    }
 }
 else {
-   Write-Information "WinGet in decent state, moving to executing DSC"
+   Write-Host "WinGet in decent state, moving to executing DSC"
 }
